db126e0ba170aa16302d3385becb459694774161
wh1tly337 <wh1tly337@gmail.com>
2022-08-02 12:44:13 +0500

    Testing v2.4.1 req_data commit

---
+++ req_data.py
@@ -0,0 +1,9 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+}
+
+host = '127.0.0.1'
+user = 'user'
+password = '13579001Ivan+'
+db_name = 'postgres'
